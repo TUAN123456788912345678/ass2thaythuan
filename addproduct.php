ecd496216bcb99ffc9079b0c7f3cd0b5324dc857
TUAN123456788912345678 <105089726+TUAN123456788912345678@users.noreply.github.com>
2022-07-03 16:35:36 +0700

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -243,6 +243,7 @@
                     </form>
                     <?PHP
                         $connect = mysqli_connect("3.132.234.157","tuan01","123@123a","tuan01");
+			mysqli_set_charset($connect, 'UTF8');
 		                if($connect)
                         {
             
